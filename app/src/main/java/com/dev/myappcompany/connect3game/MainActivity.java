b3417e0050fc5e9a0dec0df37a5c817cb2aa1529
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-17 13:57:58 -0300

    adding yellow pieces animations in board

--- app/src/main/java/com/dev/myappcompany/connect3game/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/connect3game/MainActivity.java
@@ -3,8 +3,22 @@ package com.dev.myappcompany.connect3game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
