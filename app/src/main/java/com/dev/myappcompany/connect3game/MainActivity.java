d2a22ec9e1e0c8100c656cf2c87f82239a9161ce
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-18 13:50:10 -0300

    implementing loop for verify winner of match

--- app/src/main/java/com/dev/myappcompany/connect3game/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/connect3game/MainActivity.java
@@ -3,18 +3,39 @@ package com.dev.myappcompany.connect3game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 0: yellow, 1: red
+    // 0: yellow, 1: red, 2: empty
+
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {5, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}
+    };
 
     int activePlayer = 0;
 
     public void dropIn(View view) {
 
         ImageView counter = (ImageView) view;
+
+        Log.i("Info", counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        gameState[tappedCounter] = activePlayer;
 
         counter.setTranslationY(-1500);
 
@@ -31,6 +52,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+        for (int[] winningPosition : winningPositions) {
+
+            if (
+                    gameState[winningPosition[0]] == gameState[winningPosition[1]]
+                            &&
+                            gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                            &&
+                            gameState[winningPosition[0]] != 2
+            ) {
+
+                Toast.makeText(this, "Someone has won! Congrats!", Toast.LENGTH_SHORT).show();
+            }
+        }
 
     }
 
